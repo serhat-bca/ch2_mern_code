7b8e1d57f7fcb865276466efa9c5952f2f460ebe
serhat-bca <sersen@bergen.org>
2024-09-30 14:05:14 -0400

    jokes_array

---
+++ Shared-Files/JS_Jokes/jokes.jsx
@@ -0,0 +1,42 @@
+const jokesArray = [
+  {
+    joke: "Why did the JavaScript developer quit his job? Because he didn't get arrays!",
+    likes: 0,
+  },
+  {
+    joke: "Why do JavaScript developers prefer promises over callbacks? Because they don't like to be kept waiting!",
+    likes: 0,
+  },
+  {
+    joke: "What's the object-oriented way to become wealthy in JavaScript? Inheritance!",
+    likes: 0,
+  },
+  {
+    joke: "Why did the JavaScript function go to therapy? It had too many anonymous functions calling it!",
+    likes: 0,
+  },
+  {
+    joke: "What's a programmer's favorite hangout place? Foo Bar!",
+    likes: 0,
+  },
+  {
+    joke: "Why did the JavaScript developer go broke? Because he lost his prototypes!",
+    likes: 0,
+  },
+  {
+    joke: "Why did the function and the array go to couples therapy? They had trouble understanding each other's scope!",
+    likes: 0,
+  },
+  {
+    joke: "Why do JavaScript programmers prefer the dark mode? Because they like to code with reduced light pollution!",
+    likes: 0,
+  },
+  {
+    joke: "What's a JavaScript developer's favorite dance move? The Callback!",
+    likes: 0,
+  },
+  {
+    joke: "Why did the JavaScript code go to rehab? Because it had too many dependencies!",
+    likes: 0,
+  },
+];
