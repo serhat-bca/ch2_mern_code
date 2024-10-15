6d5058ef53db53a99ee710d37621f1bd0e0bab26
serhat-bca <sersen@bergen.org>
2024-10-15 15:53:31 -0400

    push useeffect

--- movies-json/src/App.jsx
+++ movies-json/src/App.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  // const moviesData = axios.get("http://localhost:3001/movies");
+  // console.log(moviesData);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/movies").then((res) => {
+      setMovies(res.data);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Movies Application</h1>
+      {movies.map((m) => (
+        <p key={m.id}>{m.title}</p>
+      ))}
     </div>
   );
 };
